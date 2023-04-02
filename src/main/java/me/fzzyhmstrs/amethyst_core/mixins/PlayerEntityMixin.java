aeacfe21423396992a67c0f6c772c2df64509637
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-04-02 00:55:24 -0400

    add new event for random syncing and add Player XP attribute

--- src/main/java/me/fzzyhmstrs/amethyst_core/mixins/PlayerEntityMixin.java
+++ src/main/java/me/fzzyhmstrs/amethyst_core/mixins/PlayerEntityMixin.java
@@ -1,9 +1,14 @@
 package me.fzzyhmstrs.amethyst_core.mixins;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReceiver;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import com.mojang.authlib.GameProfile;
 import me.fzzyhmstrs.amethyst_core.interfaces.SpellCastingEntity;
 import me.fzzyhmstrs.amethyst_core.interfaces.SyncedRandomProviding;
 import me.fzzyhmstrs.amethyst_core.item_util.AbstractAugmentBookItem;
+import me.fzzyhmstrs.amethyst_core.registry.RegisterAttribute;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -14,6 +19,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
@@ -30,5 +37,19 @@ public class PlayerEntityMixin implements SyncedRandomProviding, SpellCastingEnt
     @Override
     public AbstractAugmentBookItem.SyncedRandomProvider getProvider() {
         return provider;
+    }
+
+    @ModifyVariable(method = "addExperience", at = @At("LOAD"), argsOnly = true)
+    private int amethyst_core_modifyXpUsingAttribute(int original){
+        int newXp = original;
+        double bonus = (int)((PlayerEntity)(Object) this).getAttributeValue(RegisterAttribute.INSTANCE.getPLAYER_EXPERIENCE());
+        while (bonus > 1.0){
+            newXp += 1;
+            bonus -= 1.0;
+        }
+        if (bonus > 0.0){
+            newXp += ((PlayerEntity)(Object) this).world.random.nextDouble() < bonus ? 1 : 0;
+        }
+        return newXp;
     }
 }
