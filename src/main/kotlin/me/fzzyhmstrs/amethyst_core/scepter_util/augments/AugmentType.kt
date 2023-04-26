6caaf94b79cd3c0bdfc426701e096d4769e6913a
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-04-26 14:26:18 -0400

    Create AugmentType.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter_util/augments/AugmentType.kt
@@ -0,0 +1,15 @@
+package me.fzzyhmstrs.amethyst_core.scepter_util.augments
+
+class AugmentType{
+
+    companion object{
+        val BOLT = AugmentType()
+        val SINGLE_TARGET_OR_SELF = AugmentType()
+        val SINGLE_TARGET = AugmentType()
+        val SLASH = AugmentType()
+        val AOE_POSITIVE = AugmentType()
+        val AOE_NEGATIVE = AugmentType()
+        val SUMMON = AugmentType()
+    }
+
+}
