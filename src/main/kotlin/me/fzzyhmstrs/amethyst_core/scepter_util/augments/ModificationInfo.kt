abef4388aba27d9739c913eabe55cc6a2a79be32
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-04-26 10:14:39 -0400

    Update ModificationInfo.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter_util/augments/ModificationInfo.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter_util/augments/ModificationInfo.kt
@@ -4,7 +4,12 @@ import me.fzzyhmstrs.fzzy_core.coding_util.PerLvlI
 
 class ModificationInfo private constructor(
     val cooldownType: ModificationType, val cooldownModifier: PerLvlI,
-    val pairingCostMultiplier: Float) {
+    val pairingCostMultiplier: Float,
+    val damageModificationType: ModificationType,
+    val amplifierModificationType: ModificationType,
+    val durabilityModificationType: ModificationType,
+    val rangeModificationType: ModificationType
+) {
 
     companion object{
         fun empty(): ModificationInfo{
