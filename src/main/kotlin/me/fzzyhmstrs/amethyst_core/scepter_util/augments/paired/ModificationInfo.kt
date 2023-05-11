695bf5bb0d232d1760d774e4fb2771632a145241
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-05-11 09:55:06 -0400

    Update ModificationInfo.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter_util/augments/paired/ModificationInfo.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter_util/augments/paired/ModificationInfo.kt
@@ -25,16 +25,62 @@ class ModificationInfo private constructor(
             )
         }
 
-        class Builder{
-            var cooldownType: ModificationType = ModificationType.DEFER
-            var cooldownModifier: PerLvlI = PerLvlI()
-            var manaCostType: ModificationType = ModificationType.DEFER
-            var manaCostModifier: PerLvlI = PerLvlI()
-            var pairingCostMultiplier: Float = 1f
-            var damageModificationType: ModificationType = ModificationType.DEFER
-            var amplifierModificationType: ModificationType = ModificationType.DEFER
-            var durationModificationType: ModificationType = ModificationType.DEFER
-            var rangeModificationType: ModificationType = ModificationType.DEFER
+        class Builder(multiplier: Float = 1f){
+            private var cooldownType: ModificationType = ModificationType.DEFER
+            private var cooldownModifier: PerLvlI = PerLvlI()
+            private var manaCostType: ModificationType = ModificationType.DEFER
+            private var manaCostModifier: PerLvlI = PerLvlI()
+            private var pairingCostMultiplier: Float = multiplier
+            private var damageModificationType: ModificationType = ModificationType.DEFER
+            private var amplifierModificationType: ModificationType = ModificationType.DEFER
+            private var durationModificationType: ModificationType = ModificationType.DEFER
+            private var rangeModificationType: ModificationType = ModificationType.DEFER
+            
+            fun setCooldownType(type: ModificationType): Builder{
+                this.cooldownType = type
+                return this
+            }
+            fun withCooldownMod(base: Int = 0, perLvl: Int = 0, percent: Int = 0):Builder{
+                cooldownModifier = PerLvlI(base,perLvl,percent)
+                return this
+            }
+            fun setManaCostType(type: ModificationType): Builder{
+                this.manaCostType = type
+                return this
+            }
+            fun withManaCostMod(base: Int = 0, perLvl: Int = 0, percent: Int = 0):Builder{
+                manaCostModifier = PerLvlI(base,perLvl,percent)
+                return this
+            }
+            fun setDamageType(type: ModificationType): Builder{
+                this.damageModificationType = type
+                return this
+            }
+            fun setAmplifierType(type: ModificationType): Builder{
+                this.amplifierModificationType = type
+                return this
+            }
+            fun setDurationType(type: ModificationType): Builder{
+                this.durationModificationType = type
+                return this
+            }
+            fun setRangeType(type: ModificationType): Builder{
+                this.rangeModificationType = type
+                return this
+            }
+            
+            fun build(): ModificationInfo{
+                return ModificationInfo(
+                    cooldownType, 
+                    cooldownModifier,
+                    manaCostType, 
+                    manaCostModifier,
+                    pairingCostMultiplier,
+                    damageModificationType,
+                    amplifierModificationType,
+                    durationModificationType,
+                    rangeModificationType)
+            }
         }
 
     }
