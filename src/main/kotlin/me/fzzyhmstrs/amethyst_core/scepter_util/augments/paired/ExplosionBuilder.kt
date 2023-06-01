492a949c3f6fad0f12cb08230c4fc87f0b212f63
fzzyhmstrs <fzzyhmstrs@gmail.com>
2023-05-31 21:27:11 -0400

    incorporate custom behavior into explosion builder

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter_util/augments/paired/ExplosionBuilder.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter_util/augments/paired/ExplosionBuilder.kt
@@ -1,9 +1,11 @@
 package me.fzzyhmstrs.amethyst_core.scepter_util.augments.paired
 
+import me.fzzyhmstrs.amethyst_core.scepter_util.augments.CustomExplosion
 import net.minecraft.entity.Entity
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 import net.minecraft.world.explosion.EntityExplosionBehavior
+import net.minecraft.world.explosion.Explosion
 import net.minecraft.world.explosion.ExplosionBehavior
 import java.util.function.Consumer
 import java.util.function.Function
@@ -13,8 +15,9 @@ class ExplosionBuilder(damageSourceBuilder: DamageSourceBuilder, val source: Ent
     private var behavior: ExplosionBehavior = if(source != null) EntityExplosionBehavior(source) else ExplosionBehavior()
     private var damageSource: DamageSourceBuilder = damageSourceBuilder
     private var power: Float = 1f
-    private var bl: Boolean = false
+    private var createFire: Boolean = false
     private var type: World.ExplosionSourceType = World.ExplosionSourceType.NONE
+    private var customBehavior: CustomExplosion.CustomExplosionBehavior = CustomExplosion.CustomExplosionBehavior()
     
     fun withBehavior(behavior: ExplosionBehavior): ExplosionBuilder {
         this.behavior = behavior
@@ -32,19 +35,23 @@ class ExplosionBuilder(damageSourceBuilder: DamageSourceBuilder, val source: Ent
         this.power = modification.apply(power)
         return this
     }
-    fun withBl(bl: Boolean): ExplosionBuilder {
-        this.bl = bl
+    fun withCreateFire(createFire: Boolean): ExplosionBuilder {
+        this.createFire = createFire
         return this
     }
     fun withType(type: World.ExplosionSourceType): ExplosionBuilder {
         this.type = type
         return this
     }
+    fun withCustomBehavior(customBehavior: CustomExplosion.CustomExplosionBehavior): ExplosionBuilder{
+        this.customBehavior = customBehavior
+        return this
+    }
     fun getPower(): Float{
         return this.power
     }
     fun explode(world: World){
-        world.createExplosion(source, damageSource.build(),behavior,pos,power,bl,type)
+        world.createExplosion(source, damageSource.build(),behavior,pos,power,createFire,type)
     }
   
 }
