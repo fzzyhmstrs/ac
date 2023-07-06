9cc6211c809d0c3c6eaee111cc54eccd7b759f83
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-07-06 13:21:20 -0400

    Update ScepterAugment.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/augments/ScepterAugment.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/augments/ScepterAugment.kt
@@ -61,8 +61,8 @@ abstract class ScepterAugment(
             return false
         }
         val effectModifiers = pairedAugments.processAugmentEffects(user, modifierData)
-        val bl = applyTasks(world,user,hand,level,effectModifiers,pairedAugments)
-        if (bl.result.isAccepted) {
+        val bl = applyTasks(world, user, hand, level, effectModifiers, pairedAugments)
+        if (bl.success()) {
             modifiers.forEach {
                 val secondary = it.getSecondaryEffect()
                 if (secondary != null) {
@@ -72,31 +72,31 @@ abstract class ScepterAugment(
                 }
             }
             effectModifiers.accept(user,AugmentConsumer.Type.AUTOMATIC)
-            AfterSpellEvent.EVENT.invoker().afterCast(world,user,user.getStackInHand(hand),bl.value, this)
+            AfterSpellEvent.EVENT.invoker().afterCast(world,user,user.getStackInHand(hand),bl.results(), this)
         }
-        return bl.result.isAccepted
+        return bl.success()
     }
 
     /**
      * The only mandatory method for extending in order to apply your spell effects. Other open functions below are available for use, but this method is where the basic effect implementation goes.
      */
-    abstract fun applyTasks(world: World, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, spells: PairedAugments): TypedActionResult<List<Identifier>>
+    abstract fun applyTasks(world: World, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, spells: PairedAugments): SpellActionResult
 
     /**
      * If your scepter has some client side effects/tasks, extend them here. This can be something like adding visual effects, or affecting a GUI, and so on.
      */
     open fun clientTask(world: World, user: LivingEntity, hand: Hand, level: Int){
     }
-    open fun onCast(context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): TypedActionResult<List<Identifier>>{
+    open fun onCast(context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): SpellActionResult{
         return SUCCESSFUL_PASS
     }
-    open fun onBlockHit(blockHitResult: BlockHitResult, context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): TypedActionResult<List<Identifier>>{
+    open fun onBlockHit(blockHitResult: BlockHitResult, context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): SpellActionResult{
         return SUCCESSFUL_PASS
     }
-    open fun onEntityHit(entityHitResult: EntityHitResult, context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): TypedActionResult<List<Identifier>>{
+    open fun onEntityHit(entityHitResult: EntityHitResult, context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): SpellActionResult{
         return SUCCESSFUL_PASS
     }
-    open fun onEntityKill(entityHitResult: EntityHitResult, context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): TypedActionResult<List<Identifier>>{
+    open fun onEntityKill(entityHitResult: EntityHitResult, context: ProcessContext, world: World, source: Entity?, user: LivingEntity, hand: Hand, level: Int, effects: AugmentEffect, othersType: AugmentType, spells: PairedAugments): SpellActionResult{
         return SUCCESSFUL_PASS
     }
 
@@ -380,130 +380,8 @@ abstract class ScepterAugment(
     }
 
     companion object{
-
-        const val augmentVersion = "_v2"
-        private const val oldAugmentVersion = "_v1"
-
-        val FAIL = actionResult(ActionResult.FAIL)
-        val SUCCESSFUL_PASS = actionResult(ActionResult.SUCCESS)
-
-        fun actionResult(result: ActionResult,oldResult: TypedActionResult<List<Identifier>>,vararg ids: Identifier): TypedActionResult<List<Identifier>>{
-            val list: MutableList<Identifier> = mutableListOf(*ids)
-            list.addAll(oldResult.value)
-            return actionResult(result,list)
-        }
-        fun actionResult(result: ActionResult,ids: List<Identifier>, oldResult: TypedActionResult<List<Identifier>>): TypedActionResult<List<Identifier>>{
-            val list: MutableList<Identifier> = ids.toMutableList()
-            list.addAll(oldResult.value)
-            return actionResult(result,list)
-        }
-        fun actionResult(result: ActionResult,vararg ids: Identifier): TypedActionResult<List<Identifier>>{
-            return actionResult(result,ids.asList())
-        }
-        fun actionResult(result: ActionResult,ids: List<Identifier>): TypedActionResult<List<Identifier>>{
-            return when(result){
-                ActionResult.SUCCESS -> TypedActionResult.success(ids)
-                ActionResult.CONSUME -> TypedActionResult.consume(ids)
-                ActionResult.CONSUME_PARTIAL -> TypedActionResult.consume(ids)
-                ActionResult.PASS -> TypedActionResult.pass(ids)
-                ActionResult.FAIL -> TypedActionResult.fail(ids)
-            }
-        }
-        
-        //small config class for syncing purposes
-        /*class AugmentConfig(val id: String, stats: AugmentStats): SyncedConfigHelper.SyncedConfig{
-
-            private var augmentStats: AugmentStats = stats
-
-            init{
-                initConfig()
-            }
-
-            override fun readFromServer(buf: PacketByteBuf) {
-                augmentStats = gson.fromJson(buf.readString(), AugmentStats::class.java).validate()
-                val augment = Registries.ENCHANTMENT.get(Identifier(id))
-                if (augment != null && augment is ScepterAugment){
-                    val currentDataPoint = augment.augmentData
-                    val newDataPoint = currentDataPoint.copy(
-                        cooldown = augmentStats.getCooldown(),
-                        manaCost = augmentStats.manaCost,
-                        minLvl = augmentStats.minLvl,
-                        castXp = augmentStats.castXp,
-                        enabled = augmentStats.enabled,
-                        pvpMode = augmentStats.pvpMode
-                    )
-                    augment.augmentData = newDataPoint
-                }
-            }
-
-            override fun writeToClient(buf: PacketByteBuf) {
-                buf.writeString(gson.toJson(augmentStats))
-            }
-
-            override fun initConfig() {
-                SyncedConfigRegistry.registerConfig(id,this)
-            }
-        }
-
-
-        class AugmentStats {
-            var id: String = AC.fallbackId.toString()
-            var enabled: Boolean = true
-            var pvpMode: Boolean = false
-            var cooldownBase: Int = 20
-            var coolDownPerLvl: Int = 0
-            var manaCost: Int = 2
-            var minLvl: Int = 1
-            var castXp: Int = 1
-
-            fun setCooldown(cooldown: PerLvlI){
-                cooldownBase = cooldown.base
-                coolDownPerLvl = cooldown.perLevel
-            }
-
-            fun getCooldown(): PerLvlI{
-                return PerLvlI(cooldownBase,coolDownPerLvl,0)
-            }
-
-            fun validate(): AugmentStats {
-                if (cooldownBase < 0) cooldownBase = 0
-                if (cooldownBase == 0 && coolDownPerLvl < 0) coolDownPerLvl = 0
-                if (manaCost < 0) manaCost = 0
-                if (minLvl < 1) minLvl = 1
-                if (castXp < 0) castXp = 0
-                return this
-            }
-        }
-
-        class AugmentStatsV1: SyncedConfigHelper.OldClass<AugmentStats> {
-            var id: String = AC.fallbackId.toString()
-            var enabled: Boolean = true
-            var cooldown: Int = 20
-            var manaCost: Int = 2
-            var minLvl: Int = 1
-            override fun generateNewClass(): AugmentStats {
-                val augmentStats = AugmentStats()
-                augmentStats.id = id
-                augmentStats.enabled = enabled
-                augmentStats.cooldownBase = cooldown
-                augmentStats.manaCost = manaCost
-                augmentStats.minLvl = minLvl
-                return augmentStats.validate()
-            }
-        }
-
-        fun configAugment(file: String, configClass: AugmentStats): AugmentStats {
-            val ns = Identifier(configClass.id).namespace
-            val oldFile = file.substring(0,file.length - 8) + oldAugmentVersion + ".json"
-            val base = if(ns == "minecraft"){
-                AC.MOD_ID
-            } else {
-                ns
-            }
-            val configuredStats = readOrCreateUpdated(file, oldFile,"augments", base, configClass = {configClass}, previousClass = { AugmentStatsV1() })
-            @Suppress("UNUSED_VARIABLE") val config = AugmentConfig(file,configuredStats)
-            return configuredStats
-        }*/
+        val FAIL = SpellActionResult.fail()
+        val SUCCESSFUL_PASS = SpellActionResult.pass()
     }
 
 
