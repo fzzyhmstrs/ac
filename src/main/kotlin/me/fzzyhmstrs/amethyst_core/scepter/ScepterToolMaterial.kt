7120f3b425e1267bfa976c3a13231185e2802e3e
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-02 11:38:27 -0400

    Update ScepterToolMaterial.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter/ScepterToolMaterial.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/scepter/ScepterToolMaterial.kt
@@ -6,10 +6,47 @@ import net.minecraft.item.ToolMaterial
 /**
  * A tool material for defining a scepter. The Scepter tier is used to determine which augments can be applied to which scepters. See [LoreTier] for more info.
  */
-abstract class ScepterToolMaterial: ToolMaterial, ManaItemMaterial {
-   
-  abstract fun scepterTier(): Int
+open class ScepterToolMaterial protected constructor(
+    private val tier: Int,
+    attackSpeedDefault: ValidatedDouble,
+    durabilityDefault: ValidatedInt,
+    miningSpeedDefault: ValidatedFloat,
+    attackDamageDefault: ValidatedFloat,
+    miningLevelDefault: ValidatedInt,
+    enchantabilityDefault: ValidatedInt,
+    repairIngredientDefault: ValidatedIngredient)
+: 
+ValidatedToolMaterial(
+    durabilityDefault,
+    miningSpeedDefault,
+    attackDamageDefault,
+    miningLevelDefault,
+    enchantabilityDefault,
+    repairIngredientDefault)
+,
+ManaItemMaterial 
+{
+    var attackSpeed = attackSpeedDefault
+    
+    fun scepterTier(): Int{
+        return tier
+    }
 
-  abstract fun getAttackSpeed(): Double
+    fun getAttackSpeed(): Double{
+        return attackSpeed.get()
+    }
+
+    open class Builder(private val tier: Int): ValidatedToolMaterial.Builder(){
+        protected var aS = ValidatedFloat(-3f,0f,-4f)
+
+        fun attackSpeed(default: Float): Builder{
+            aS = ValidatedFloat(default,0f,-4f)
+        }
+
+        override fun build(): ScepterToolMaterial{
+            return ScepterToolMaterial(tier, aS, d, mSM, aD, mL, e, rI)
+        }
+        
+    }
   
 }
