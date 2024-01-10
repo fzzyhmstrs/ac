a42697d6688867767927b7bd1aac1237fd83a54f
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-01-10 08:53:19 -0500

    Update RegisterBaseEntity.kt

--- src/main/kotlin/me/fzzyhmstrs/amethyst_core/registry/RegisterBaseEntity.kt
+++ src/main/kotlin/me/fzzyhmstrs/amethyst_core/registry/RegisterBaseEntity.kt
@@ -2,6 +2,7 @@ package me.fzzyhmstrs.amethyst_core.registry
 
 import me.fzzyhmstrs.amethyst_core.AC
 import me.fzzyhmstrs.amethyst_core.entity_util.MissileEntity
+import me.fzzyhmstrs.fzzy_core.coding_util.FzzyPort
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
@@ -13,8 +14,7 @@ import net.minecraft.world.World
 
 object RegisterBaseEntity {
 
-    val MISSILE_ENTITY: EntityType<MissileEntity> = Registry.register(
-        Registries.ENTITY_TYPE,
+    val MISSILE_ENTITY: EntityType<MissileEntity> = FzzyPort.ENTITY_TYPE.register(
         Identifier(AC.MOD_ID, "missile_entity"),
         FabricEntityTypeBuilder.create(
             SpawnGroup.MISC
